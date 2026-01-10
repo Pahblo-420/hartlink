296b321b92ff8f12c91338c2d2320e9f4e451c4a
Pahblo-420 <lipopopaballo022@gmail.com>
2026-01-10 09:19:16 +0200

    fixed css for features section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
         nav.classList.toggle("active");
     });
 });
+document.querySelectorAll(".hero-buttons a").forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.style.transform = "scale(0.95)";
+        setTimeout(() => btn.style.transform = "scale(1)", 150);
+    });
+});
